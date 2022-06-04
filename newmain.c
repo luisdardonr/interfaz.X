463af10fad3ce8655a092f94d63bb070fd69023c
luisdardonr <dar20392@uvg.edu.gt>
2022-06-03 20:40:10 -0600

    interfaz_proyecto_2

---
+++ newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: dellG3
+ *
+ * Created on 3 de junio de 2022, 08:37 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
